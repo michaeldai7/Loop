9049b1238547c624dfd05c1241e1f1b6ceea9fc1
michaeldai7 <michealxia7@gmail.com>
2020-03-04 17:34:34 +0000

    233

---
+++ 12.java
@@ -0,0 +1,10 @@
+import java.util.*;
+import java.io.*; 
+class Twelve {
+	public static viod main (String[] args){
+		Scanner sc = new Scanner(System.in);
+		int rows = sc.nextInt();
+		int[][] matirx;
+
+	}
+}
