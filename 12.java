8178fbb450e2dacab924bdd98fc1ecf6e06a7c1f
michaeldai7 <michealxia7@gmail.com>
2020-03-06 10:02:07 -0500

    2020-03-06

--- 12.java
+++ 12.java
@@ -1,10 +1,45 @@
 import java.util.*;
-import java.io.*; 
+//import java.io.*;
+
 class Twelve {
-	public static viod main (String[] args){
+	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int rows = sc.nextInt();
-		int[][] matirx;
+		int[][] matrix = new int[rows][rows];
+		int length = 0;
+		int x = 0; //hang
+		int y = 0;// lie
+		int z = 0;// 
+		int a = 0;
+		int b = 0;
+		for (int i = 0; i < matrix.length; i++) {
 
+			/*while (z < matrix.length / 2) {
+				System.out.print(" ");
+				z++;
+			}*/
+			if (y == 0) {
+				matrix[x][y] = 1;
+			}
+			if (x % 2 == 0) {
+				matrix[x][(y/2)+a] =
+			}
+
+
+
+
+
+
+
+
+
+			if (x % 2 == 0) {
+
+			}
+			x++;
+			z = 0;
+			y = 0;
+
+		}
 	}
 }
---
+++ test.java
@@ -0,0 +1,13 @@
+import java.io.PrintStream;
+
+/**
+ * test
+ */
+public class test {
+    public static void main(String[] args) {
+        for(;;){
+            System.out.print("rua ");
+        }
+    }
+    
+}
